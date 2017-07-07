f53f2f986c244963f348f20e10a658ea794ec2ca
Thunnathorn <phantomzmc@gmail.com>
2017-07-07 09:58:21 +0700

    Initial Commit

---
+++ TestApp2Tests/TestApp2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TestApp2Tests.swift
+//  TestApp2Tests
+//
+//  Created by Thannathrn Yuwasin on 7/7/2560 BE.
+//  Copyright © 2560 Thannathrn Yuwasin. All rights reserved.
+//
+
+import XCTest
+@testable import TestApp2
+
+class TestApp2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
